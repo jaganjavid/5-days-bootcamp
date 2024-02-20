2b634e072460a7296f33675c282ef924c862b27c
jaganjavid <jagantutor1@gmail.com>
2024-02-20 18:52:05 +0530

    React Started

---
+++ blog/client/src/App.js
@@ -0,0 +1,10 @@
+import Header from "./compoents/Header";
+function App() {
+  return (
+   <div>
+    <Header/> 
+   </div>
+  );
+}
+
+export default App;
--- js/dom/app.js
+++ js/dom/app.js
@@ -7,11 +7,7 @@ const clearBtn = document.querySelector(".clear-tasks");
 
 
 // Load All Event Listeners
-
 function loadEventListeners(){
-
-    // DOM LOAD EVENT
-    document.addEventListener("DOMContentLoaded", getTask);
 
     // Add Task Event
     form.addEventListener("submit", addTask);
@@ -23,46 +19,10 @@ function loadEventListeners(){
     taskList.addEventListener("click", removeTask)
 }
 
-function getTask(){
-    let tasks;
-
-    if(localStorage.getItem("tasks") === null){
-      tasks = [];
-    }else{
-       tasks = JSON.parse(localStorage.getItem("tasks"));
-    }
-
-    tasks.forEach(function(element){
-      
-    // Create li Element
-    const li = document.createElement("li");
-
-    // Add a class name
-    li.className = "collection-item";
-
-    // Create a Text node
-    li.appendChild(document.createTextNode(element));
-
-    // Create a link
-    const link = document.createElement("a");
-
-    // add a class to link
-    link.className = "delete-item secondary-content";
-
-    // Add a icon to link
-    link.innerHTML = `<i class="fa fa-remove"></i>`;
- 
-    // add link to li
-    li.appendChild(link);
-
-    // Add li to ul
-    taskList.appendChild(li);
-
-    })
-}
 
 function addTask(event){
-    event.preventDefault();
+
+  event.preventDefault();
     
    if(taskInput.value === ""){
      alert("Please Fill the form");
@@ -92,32 +52,11 @@ function addTask(event){
     // Add li to ul
     taskList.appendChild(li);
 
-    storeTaskInLocalStroge(taskInput.value);
-
     // clear the input value
     taskInput.value = "";
 
    }
 }
-
-function storeTaskInLocalStroge(task){
-   
-
-    let tasks;
-
-    if(localStorage.getItem("tasks") === null){
-      tasks = [];
-    }else{
-       tasks = JSON.parse(localStorage.getItem("tasks"));
-    }
-
-    tasks.push(task);
-
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-
-
-}
-
 
 
 function removeTask(event){
@@ -135,15 +74,3 @@ function clearTask() {
 
 loadEventListeners();
 
-
-// const arr = [1,2,3];
-
-// // Chage arr to string
-
-// const str = JSON.stringify(arr)
-
-
-
-// const arrBack = JSON.parse(str);
-// console.log(arrBack);
-// console.log(typeof arrBack);
